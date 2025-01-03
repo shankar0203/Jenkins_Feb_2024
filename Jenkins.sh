44f8ade31171c9af458f28a29800f985da6ccafc
shankar0203 <79186682+shankar0203@users.noreply.github.com>
2025-01-03 11:47:33 +0530

    Update Jenkins.sh

--- Jenkins.sh
+++ Jenkins.sh
@@ -1,4 +1,17 @@
-pwd
-date
-mkdir test
-ls
+# Use an official lightweight Linux base image
+FROM ubuntu:20.04
+
+# Set environment variables to suppress interactive prompts during installation
+ENV DEBIAN_FRONTEND=noninteractive
+
+# Update package list and install Apache HTTP Server
+RUN apt-get update && apt-get install -y apache2 && apt-get clean
+
+# Change Apache's listening port to 90
+RUN sed -i 's/80/90/g' /etc/apache2/ports.conf /etc/apache2/sites-available/000-default.conf
+
+# Expose port 90
+EXPOSE 90
+
+# Start the Apache HTTP Server in the foreground
+CMD ["apachectl", "-D", "FOREGROUND"]
